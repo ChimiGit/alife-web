348de817f14d0e8008ea290d867f8c7702e17861
ChimiGit <ctsheringgit208@gmail.com>
2025-09-12 00:36:55 +0600

    :rocket: fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://ChimiGit.github.io/alife-web',
+  site: 'https://ChimiGit.github.io',
   base: '/alife-web/',
   i18n: {
     defaultLocale: 'en',
