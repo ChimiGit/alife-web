6e9386ecc8e545da318c2513299705fb411e0411
ChimiGit <ctsheringgit208@gmail.com>
2025-09-25 15:50:14 +0600

    feat: make people page dynamic and remove supporters section
    - Dynamically generate team sections based on team-categories collection
    - Automatically sort categories by order field
    - Only display categories that have team members
    - Remove 'Meet Our Supporters' section as requested
    - Remove unused imports (SupporterSection, support utilities)
    - Make the page scalable for adding new team categories and members

---
+++ dist/assets/TeamSection.BkTOFIz6.js
@@ -0,0 +1,1 @@
+import{T as a}from"./TeamSection.BRRWX8OB.js";import"./index.BdsVreIv.js";export{a as default};
---
+++ dist/assets/index.DTuQk29Q.js
@@ -0,0 +1,1 @@
+import{g as a,a as e,c as t}from"./TeamSection.BRRWX8OB.js";import"./index.BdsVreIv.js";const i={renderer:t,...e,...a};var r=i;export{r as default};
